c1564fe0a5d68576fe25ee9122b6326f4b5178b0
Robert Giesecke <robert.giesecke@gmail.com>
2026-05-09 00:54:11 +0200

    Remove `ScaledAxisCSharp` project and associated files, including runtime, program logic, and configuration files. Clean up unused references and related solution entries.

--- ScaledAxisCSharp/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿// Global using directives
-
-global using System.Runtime.CompilerServices;
-global using System.Runtime.InteropServices;
-global using ScaledAxisCSharp.Config;
-global using ScaledAxisCSharp.DirectInput;
-global using ScaledAxisCSharp.VJoy;
