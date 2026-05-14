eb5871a1984911e0fee242d46fe117734056abd2
Robert Giesecke <robert.giesecke@gmail.com>
2026-05-14 19:24:53 +0200

    Use renamed vJoy outputs in ItbMinimal sample

--- ScaledAxisCSharp.ItbMinimal/Program.cs
+++ ScaledAxisCSharp.ItbMinimal/Program.cs
@@ -1,19 +1,16 @@
-using var connectedDevices = DirectInputJoystickDevice.EnumerateConnected();
-
-var rightStick = connectedDevices.ResolveDevice("RIGHT VPC Stick WarBRD");
-var leftStick = connectedDevices.ResolveDevice("LEFT VPC Stick WarBRD");
+using static Devices.Typed;
 
 var modifierBlendCurve = new BlendedAxisCurve
 {
 	NormalCurve = new AxisCurve { Max = 1.0d },
 	PrecisionCurve = new AxisCurve { Max = 0.184d },
-	ModifierAxis = leftStick.BindAxis(PhysicalAxis.Slider1 /*brake lever*/),
+	ModifierAxis = LeftStick.Axes.BrakeLever,
 };
 
 // 50% when left 2nd stage trigger is pressed, blended otherwise
 var blendedCurveWithPrecisionHold = new WhenButtonPressedAxisModifier
 {
-	Buttons = [leftStick.BindButton(2 /*2nd stage trigger*/)],
+	Buttons = [LeftStick.Buttons.SecondStageTrigger],
 	WhenPressed = new AxisCurve { Max = 0.5d },
 	WhenNotPressed = modifierBlendCurve,
 };
@@ -23,7 +20,9 @@ var axisOptions = new RouteAxisOptions
 	Modifier = blendedCurveWithPrecisionHold,
 };
 
-Runtime.BuildAndRunAsConsole(new()
+using var connectedDevices = DirectInputJoystickDevice.EnumerateConnected();
+
+BuildAndRunAsConsole(new()
 {
 	Name = "ItB minimal + scaled rotations",
 	ConnectedDevices =
@@ -32,30 +31,37 @@ Runtime.BuildAndRunAsConsole(new()
 	],
 	ButtonRoutes =
 	[
-		new(rightStick.BindButton(1 /*trigger*/),
-			1,
-			1),
-		new(leftStick.BindButton(1 /*trigger*/),
-			1,
-			40),
-		new(leftStick.BindButton(11 /*outer 2-way up*/),
-			1,
-			79),
-		new(rightStick.BindButton(18) /*cm hat east*/,
-			1,
-			22),
+		RightStick.Buttons.Trigger.RouteTo(VJoyLeft.Buttons.Fire),
+		LeftStick.Buttons.Trigger.RouteButton(1, 40),
+		LeftStick.Buttons.Outer2WayUp.RouteTo(VJoyLeft.Buttons.CenterHeadTracking),
+		RightStick.Buttons.CounterMeasureHatEast.RouteButton(1, 22),
 	],
 	AxisRoutes =
 	[
-		rightStick.BindAxis(PhysicalAxis.X)
-			.RouteToSameAxisOnOutput(1,
-				options: axisOptions),
-		rightStick.BindAxis(PhysicalAxis.Y)
-			.RouteToSameAxisOnOutput(1,
-				options: axisOptions),
-		rightStick.BindAxis(PhysicalAxis.Z)
-			.RouteToSameAxisOnOutput(1,
-				options: axisOptions),
+		RightStick.Axes.X.RouteToSameAxisOnOutput(1, options: axisOptions),
+		RightStick.Axes.Y.RouteToSameAxisOnOutput(1, options: axisOptions),
+		RightStick.Axes.Twist.RouteToSameAxisOnOutput(1, options: axisOptions),
 	],
 	OutputDeviceFactory = VJoyDeviceFactory.Instance,
 });
+
+
+[GenerateDeviceInfos(GenerateDeviceInfosLevels.All)]
+//
+[RenameDevice(DeviceNames.RightVpcStickWarBRD, "RightStick")]
+[RenameAxis(DeviceNames.RightVpcStickWarBRD, PhysicalAxis.Z, "Twist")]
+[RenameButton(DeviceNames.RightVpcStickWarBRD, 1, "Trigger")]
+[RenameButton(DeviceNames.RightVpcStickWarBRD, 18, "CounterMeasureHatEast")]
+//
+[RenameDevice(DeviceNames.LeftVpcStickWarBRD, "LeftStick")]
+[RenameAxis(DeviceNames.LeftVpcStickWarBRD, PhysicalAxis.Slider1, "BrakeLever")]
+[RenameButton(DeviceNames.LeftVpcStickWarBRD, 1, "Trigger")]
+[RenameButton(DeviceNames.LeftVpcStickWarBRD, 2, "SecondStageTrigger")]
+[RenameButton(DeviceNames.LeftVpcStickWarBRD, 11, "Outer2WayUp")]
+[RenameButton(DeviceNames.LeftVpcStickWarBRD, 20, "BrakeLever")]
+//
+[RenameDevice(DeviceNames.VJoyDevice1, "VJoyLeft")]
+[RenameButton(DeviceNames.VJoyDevice1, 1, "Fire")]
+[RenameButton(DeviceNames.VJoyDevice1, 79, "CenterHeadTracking")]
+// ReSharper disable once ClassNeverInstantiated.Global
+partial class Devices;
