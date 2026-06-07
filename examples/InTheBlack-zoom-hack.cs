a5d64f15d7f2d3a1e4676db348c554566982d6f1
Robert Giesecke <robert.giesecke@gmail.com>
2026-06-07 21:14:02 +0200

    Build: dedupe transitive analyzer for in-repo #:package scripts; example uses Editor bundle
    Skip a transitive-analyzer projref whose assembly is already a package-supplied Analyzer item, so an in-repo file-based script that gets the generator from both paths no longer double-emits. Exclude ItbMinimal from the Release solution build.

--- examples/InTheBlack-zoom-hack.cs
+++ examples/InTheBlack-zoom-hack.cs
@@ -1,5 +1,6 @@
-#:project ../src/Console/Console.csproj
-//#:package SharpSticks.Editor@0.1.0-linux16
+#!/usr/bin/env dotnet
+
+#:package SharpSticks.Editor@0.1.0-linux17
 
 [assembly:GenerateDeviceInfos(GenerateDeviceInfosLevels.All)]
 [assembly:RenameDevice(DeviceNames.RightVpcStickWarBRD, "RightStick")]
@@ -48,21 +49,28 @@ BuildAndRunAsConsole(new()
 		RightStick.Axes.X.RouteTo(VJoy1.Axes.Roll, modifier: modifierBlendCurve),
 		RightStick.Axes.Y.RouteTo(VJoy1.Axes.Pitch, modifier: modifierBlendCurve),
 		RightStick.Axes.Twist.RouteTo(VJoy1.Axes.Yaw, modifier: modifierBlendCurve),
-		LeftStick.Axes.BrakeLever.RouteTo(VJoy1.Axes.BrakeLever, scale: 2, offset: -1),
+		//LeftStick.Axes.BrakeLever.RouteTo(VJoy1.Axes.BrakeLever, scale: 2, offset: -1),
 		// simulate absolute zoom with 2 virtual relative axes
 		LeftStick.Axes.BrakeLever.RouteAbsoluteRelative(new()
 		{
-			IncreaseAxis = VJoy1.Axes.ZoomIn,
-			DecreaseAxis = VJoy1.Axes.ZoomIn,
-			Gain = 8.0,
-			MinOutput = 0.001,
+			IncreaseAxis = VJoy1.Axes.ZoomInOut,
+			DecreaseAxis = VJoy1.Axes.ZoomInOut,
+			// The lever is a signed axis resting at -1. The default source
+			// range is [0, 1], which throws away the first half of the pull.
+			SourceInputMinimum = -1.0,
+			SourceInputMaximum = 1.0,
+			Gain = 6.0,
+			// Must clear the game's deadzone: pulses below it advance the
+			// model but not the game, so the zoom never reaches the stops.
+			// Tune to just above where the game starts reacting.
+			MinOutput = 0.15,
 			ErrorTolerance = 0.00003,
-			IncreaseEdgeBoost = 5.5,
-			DecreaseEdgeBoost = 5.5,
+			IncreaseEdgeBoost = 15.5,
+			DecreaseEdgeBoost = 15.5,
 			OutputRiseRate = 0.14,
 			OutputFallRate = 0.14,
-			IncreaseRate = 0.035,
-			DecreaseRate = 0.035,
+			IncreaseRate = 0.025,
+			DecreaseRate = 0.025,
 		}),
 	],
 });
@@ -80,17 +88,18 @@ BuildAndRunAsConsole(new()
 [RenameButton(DeviceNames.LeftVpcStickWarBRD, 11, "Outer2WayUp")]
 [RenameButton(DeviceNames.LeftVpcStickWarBRD, 20, "BrakeLever")]
 // vjoy device
-[RenameButton(DeviceNames.VJoyDevice1, 1, "Fire")]
-[RenameButton(DeviceNames.VJoyDevice1, 79, "CenterHeadTracking")]
+[RenameButton(DeviceNames.VJoy1, 1, "Fire")]
+[RenameButton(DeviceNames.VJoy1, 79, "CenterHeadTracking")]
 
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.X, "Roll")]
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.Y, "Pitch")]
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.Z, "Yaw")]
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.Rz, "BrakeLever")]
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.Slider1, "ZoomIn")]
-[RenameAxis(DeviceNames.VJoyDevice1, Axis.Slider2, "ZoomOut")]
+[RenameAxis(DeviceNames.VJoy1, Axis.X, "Roll")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Y, "Pitch")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Z, "Yaw")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Rz, "BrakeLever")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Ry, "ZoomInOut")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Slider1, "ZoomIn")]
+[RenameAxis(DeviceNames.VJoy1, Axis.Slider2, "ZoomOut")]
 
-[RenameButton(DeviceNames.VJoyDevice1, 71, "SwitchToWeaponGroup1")]
-[RenameButton(DeviceNames.VJoyDevice1, 72, "SwitchToWeaponGroup2")]
-[RenameButton(DeviceNames.VJoyDevice1, 20, "HoldForZoom")]
+[RenameButton(DeviceNames.VJoy1, 71, "SwitchToWeaponGroup1")]
+[RenameButton(DeviceNames.VJoy1, 72, "SwitchToWeaponGroup2")]
+[RenameButton(DeviceNames.VJoy1, 20, "HoldForZoom")]
 partial class Devices;
