d82f63253f5b47a480abec62bde826f76dc619c2
Robert Giesecke <robert.giesecke@gmail.com>
2026-09-06 23:59:43 +0200

    Refactor precision-toggle example: reuse axis curve definitions, update RightStick naming, saturate Z-axis curve at 80% travel.

--- examples/precision-toggle.cs
+++ examples/precision-toggle.cs
@@ -6,17 +6,18 @@
 [assembly: RenameButton(DeviceNames.VJoyDevice1, 1, "FireWeapon1")]
 [assembly: RenameButton(DeviceNames.VJoyDevice1, 2, "FireWeapon2")]
 
-[assembly: RenameButton(DeviceNames.RightVpcStickWarBRD, 18, "CounterMeasureHatEast")]
-[assembly: RenameButton(DeviceNames.RightVpcStickWarBRD, 1, "Trigger")]
-[assembly: RenameButton(DeviceNames.RightVpcStickWarBRD, 2, "SecondStageTrigger")]
+[assembly: RenameButton(DeviceNames.RightStick, 18, "CounterMeasureHatEast")]
+[assembly: RenameButton(DeviceNames.RightStick, 1, "Trigger")]
+[assembly: RenameButton(DeviceNames.RightStick, 2, "SecondStageTrigger")]
 
 
+var axisWith80PercentSaturation = new AxisCurve { Max = 1, Saturation = 0.8 /* reach 100% at 80% stick deflection */ };
 var dualCurveAxis = new WhenButtonPressedAxisModifier
 {
 	Buttons = [LeftVpcStickWarBRD.Buttons.Btn1],
 	// Full deflection at 80% of the stick's travel; each curve runs over that range and caps at its Max.
-	WhenNotPressed = new AxisCurve { Max = 1, Saturation = 0.8 /* reach 100% at 80% stick deflection */ },
-	WhenPressed = new AxisCurve { Max = 0.5d, Saturation = 0.8 /* reach 100% at 80% stick deflection */ },
+	WhenNotPressed = axisWith80PercentSaturation,
+	WhenPressed = new AxisCurve { Max = 0.5d },
 };
 
 BuildAndRunAsConsole(new()
@@ -26,7 +27,7 @@ BuildAndRunAsConsole(new()
 	[
 		RightStick.Axes.X.RouteToSameAxisOnOutput(OutputDeviceIds.VJoyDevice1, modifier: dualCurveAxis),
 		RightStick.Axes.Y.RouteToSameAxisOnOutput(OutputDeviceIds.VJoyDevice1, modifier: dualCurveAxis),
-		RightStick.Axes.Z.RouteToSameAxisOnOutput(OutputDeviceIds.VJoyDevice1, modifier: dualCurveAxis),
+		RightStick.Axes.Z.RouteToSameAxisOnOutput(OutputDeviceIds.VJoyDevice1, modifier: axisWith80PercentSaturation),
 		RightStick.Buttons.Trigger.RouteTo(VJoyDevice1.Buttons.FireWeapon1),
 		RightStick.Buttons.SecondStageTrigger.RouteTo(VJoyDevice1.Buttons.FireWeapon2),
 		RightStick.Buttons.CounterMeasureHatEast.RouteTo(VJoyDevice1.Buttons.FireWeapon2),
